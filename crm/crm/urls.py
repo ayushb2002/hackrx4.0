8412c5ecf136ec07bbd78b225ea07ea2f33e0787
Ayush Bansal <ayushb6732@gmail.com>
2023-07-15 16:30:01 +0530

    Fetched and stored twitter data

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from .views import get_instagram_profile,get_instagram_stats,get_instagram_posts,get_subreddit_data,get_tweets
 
 urlpatterns = [
+    # username
     path('instagram/profile/', get_instagram_profile, name='instagram-profile'),
+    # array of instagram post links => post_links=[...]
     path('instagram/stats/', get_instagram_stats, name='instagram-stats'),
-     path('instagram-posts/', get_instagram_posts, name='get_instagram_posts'),
-      path('subreddit-data/', get_subreddit_data, name='get_subreddit_data'),
-       path('tweets/', get_tweets, name='get_tweets'),
+    # hashtag from instagram 
+    path('instagram-posts/', get_instagram_posts, name='get_instagram_posts'),
+    # subreddit_name
+    path('subreddit-data/', get_subreddit_data, name='get_subreddit_data'),
+    # keyword, count
+    path('tweets/', get_tweets, name='get_tweets'),
 ]
